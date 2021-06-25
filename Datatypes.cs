fe19e46e3f5ff6d10438126bd1e08a2d44daab1f
abhinay <63472859+abhinay-developer@users.noreply.github.com>
2021-06-25 16:10:15 +0530

    Create Datatypes.cs

---
+++ Datatypes.cs
@@ -0,0 +1,35 @@
+using System;
+
+namespace Helloworld
+{
+    class Program
+    {
+      
+
+        static void Main(string[] args)
+        {
+            int age = 10;
+            string name = "raja";
+            float weight = 45.6f;
+            double salary = 15000.254;
+            long mobileNumber = 98989865632;
+            bool isMarried = false;
+           
+            //To print Message on the console
+            /*
+             *This is starting of the Program
+             */
+            Console.WriteLine("The age "+age);
+            Console.WriteLine("The weight " + weight);
+
+            Console.WriteLine("The salary " + salary);
+            Console.WriteLine("The mobileNumber " + mobileNumber);
+            Console.WriteLine("The isMarried " + isMarried);
+
+
+
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
